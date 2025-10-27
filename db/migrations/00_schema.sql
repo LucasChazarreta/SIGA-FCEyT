1bd8423def43bae6c0565e9d3a520ecb011fcbbe
Lucas Chazarreta <LucasChazarreta84@yahoo.com>
2025-10-27 13:14:31 -0300

    solucion BD

--- db/migrations/00_schema.sql
+++ db/migrations/00_schema.sql
@@ -1,6 +1,10 @@
 -- Crear base
 CREATE DATABASE IF NOT EXISTS siga
-  CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+USE siga;
+
 USE siga;
 
 -- Roles
